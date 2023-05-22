304f168e94152673f3c8bbad61b2af39819a935d
matios676 <31513484+matios676@users.noreply.github.com>
2023-05-22 10:59:23 +0200

    Added collisions v1

--- modelLoader.js
+++ modelLoader.js
@@ -2,17 +2,16 @@
 //import { MTLLoader } from 'https://cdn.jsdelivr.net/npm/three@0.125.0/examples/jsm/loaders/MTLLoader.js';
 
 //GTFL + DRACO
-import { DRACOLoader } from 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/loaders/DRACOLoader.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'DRACOLoader';
+import { GLTFLoader } from 'GLTFLoader';
 
 // Optional: Provide a DRACOLoader instance to decode compressed mesh data
 const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/libs/draco/gltf/' );
+const loader = new GLTFLoader();
+//dracoLoader.setDecoderPath( 'https://cdn.jsdelivr.net/npm/three@0.150.0/examples/jsm/libs/draco/gltf/' );
+dracoLoader.setDecoderPath( './jsm_backup/draco_gltf/' );
 
 var ModelLoader = function(glb, callback) {
-        
-        // Instantiate a loader
-        const loader = new GLTFLoader();
 
         loader.setDRACOLoader( dracoLoader );
 
